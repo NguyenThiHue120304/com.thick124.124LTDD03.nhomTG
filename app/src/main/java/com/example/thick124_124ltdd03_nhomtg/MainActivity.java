4ebcf887b6fb07cabb75911f988baaf7bbd6f260
NguyenThiHue120304 <huenguyen120304@gmail.com>
2024-12-21 09:30:07 +0700

    commit

--- app/src/main/java/com/example/thick124_124ltdd03_nhomtg/MainActivity.java
+++ app/src/main/java/com/example/thick124_124ltdd03_nhomtg/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.thick124_124ltdd03_nhomtg;
 
+
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.thick124_124ltdd03_nhomtg.fragment.ViewPagerAdapter;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,51 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        viewPager = findViewById(R.id.view_pager);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position){
+                    case 0 :
+                        bottomNavigationView.getMenu().findItem(R.id.menu_home).setChecked(true);
+                        break;
+                    case 1 :
+                        bottomNavigationView.getMenu().findItem(R.id.menu_setting).setChecked(true);
+                        break;
+
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.menu_home) {
+                    viewPager.setCurrentItem(0);
+                } else if (itemId == R.id.menu_setting) {
+                    viewPager.setCurrentItem(1);
+                }
+
+                return true;
+            }
+        });
+
     }
 }
