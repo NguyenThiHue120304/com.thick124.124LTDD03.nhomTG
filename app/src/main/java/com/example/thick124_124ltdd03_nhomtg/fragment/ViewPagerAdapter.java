2b26f65c00c195a9a8c8020065fdd8db5a74548c
NguyenThiHue120304 <huenguyen120304@gmail.com>
2024-12-21 09:13:04 +0700

    init project

---
+++ app/src/main/java/com/example/thick124_124ltdd03_nhomtg/fragment/ViewPagerAdapter.java
@@ -0,0 +1,31 @@
+package com.example.thick124_124ltdd03_nhomtg.fragment;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+    public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new HomeFragment();
+
+            case 1:
+                return new ProfileFragment();
+            default:
+                return new HomeFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 1;
+    }
+}
